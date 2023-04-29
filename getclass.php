10ae38e4e6aa892f6d4028529a76629e6562bb1c
Lehotaiabel <cqde.hu@gmail.com>
2023-04-29 23:18:54 +0200

    verson

--- getclass.php
+++ getclass.php
@@ -9,7 +9,7 @@ if (!$conn) {
 }
 
 // SQL lekérdezés futtatása
-$sql = "SELECT * FROM `lessons` ORDER BY `lessons`.`sorszam` ASC";
+$sql = "SELECT * FROM `lessons` ORDER BY `lessons`.`sorszam` DESC";
 $result = mysqli_query($conn, $sql);
 
 // Ellenőrzés, hogy sikeres volt-e a lekérdezés
