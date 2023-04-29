5371558acbbc7ab14153f2e00358e952aa181366
Lehotaiabel <cqde.hu@gmail.com>
2023-04-29 18:48:23 +0200

    verson

--- main.js
+++ main.js
@@ -23,27 +23,29 @@ var m3 = $('#m3')
 //var m3 = document.createElement('div')
 //m3.className = "m-3 "
 
-var Row1 = document.createElement('div')
-Row1.className = "row justify-content-center "
+var hetfoRow = document.createElement('div')
+hetfoRow.className = "row justify-content-center "
 
-var dayCol1 = document.createElement('div')
-dayCol1.className = "col bg-light m-2 p-0 rounded "
+var hetfoCol = document.createElement('div')
+hetfoCol.className = "col bg-light m-2 p-0 rounded "
 
-var dayP1 = document.createElement('p')
-dayP1.className = "text-center TW m-0 py-3 px-2 "
-dayP1.innerHTML = "Hétfő"
+var hetfoP = document.createElement('p')
+hetfoP.className = "text-center TW m-0 py-3 px-2 "
+hetfoP.innerHTML = "Hétfő"
+
+m3.append(hetfoRow)
+hetfoRow.appendChild(hetfoCol)
+hetfoCol.appendChild(hetfoP)
 
 
-//col-1 bg-light m-2 p-0 rounded col-vertical-middle
+var hetfoClass1 = document.createElement('div')
+hetfoClass1.className = "row justify-content-center "
 
-var order = document.createElement('')
+var hetfoClass1Order = document.createElement('div')
+hetfoClass1Order.className = "col-1 bg-light m-2 p-0 rounded col-vertical-middle "
 
-hetfo.append(m3)
-m3.append(Row1)
-Row1.appendChild(dayCol1)
-dayCol1.appendChild(dayP1)
 
-m3.append(Row1)
+
 
 
 
