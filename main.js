060f4a6792f839cbe2fb44cfb270afafce6bf4c6
Lehotaiabel <cqde.hu@gmail.com>
2023-04-29 22:44:57 +0200

    verson

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ function createClass(sorszam, tantargy, terem, nap) {
     parentDiv.appendChild(childDiv);
 
 
-    $(parentDiv).appendTo("#Hétfős");
+    $(parentDiv).insertAfter("#Hétfős");
   
     
 
