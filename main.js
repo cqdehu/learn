1d6b490dcc43bdb1372b8d5b5e61cf5c99ee5ec6
Lehotaiabel <cqde.hu@gmail.com>
2023-04-29 18:24:52 +0200

    verson

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ $(document).ready(function () {
     })
 })
 
+
+
+var m3 = document.createElement('div')
+m3.className = "m-3 "
+
+var dayRow1 = document.createElement('div')
+dayRow1.className = "row justify-content-center "
+
+var dayCol1 = document.createElement('div')
+dayCol1.className = "col bg-light m-2 p-0 rounded "
+
+var dayP1 = document.createElement('p')
+dayP1.className = "text-center TW m-0 py-3 px-2 "
+dayP1.innerHTML = "Hétfő"
+
+
+$('hetfo').appendChild(m3)
+m3.appendChild(dayRow1)
+dayRow1.appendChild(dayCol1)
+dayCol1.appendChild(dayP1)
+
+
+
+
+
+
